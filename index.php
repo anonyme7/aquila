9e7c62bccd2d16eff01beaf65111845f798bf10e
olivier1322 <olomolo132@yahoo.fr>
2022-03-23 15:29:44 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * 
+ * Main template file
+ * 
+ * @package aquila
+ */
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Wordpress theme</title>
+ </head>
+ <body>
+
+    hello wordpress
+     
+ </body>
+ </html>
+
+ 
